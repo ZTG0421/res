d126b5a823c17a4f33595b515fb97a4738bd0d07
ZTG0421 <79940512+ZTG0421@users.noreply.github.com>
2024-02-20 19:35:08 +0800

    Create music.py

---
+++ music.py
@@ -0,0 +1,11 @@
+def get_music_cover(id):
+    import requests
+    from lxml import etree
+    url = 'https://music.163.com/song?id={}'.format(id)
+    head = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
+    }
+    response = requests.get(url, headers=head)
+    html = etree.HTML(response.text)
+    cover_url=html.xpath('//img/@data-src')[0]
+    return cover_url
